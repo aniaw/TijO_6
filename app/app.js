63a43fb1f7114a28a329d63511a66901f4009f7f
GKuta95 <g.kuta95@gmail.com>
2017-04-05 11:09:12 +0200

    Implementation findMax function

--- app/app.js
+++ app/app.js
@@ -1,6 +1,17 @@
 function findMax(array)
 {
+    var max = array[0];
+    var lenght = array.length;
 
+    for(var i = 0; i < lenght; i++){
+      if(isNaN(array[i])){
+          return false;
+      }
+      else if(array[i]>max){
+          max = array[i];
+      }
+    }
+    return max;
 }
 module.exports = {
     findMax: findMax
