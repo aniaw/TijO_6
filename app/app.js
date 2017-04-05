4efe11fb25e0ef39b1800c2d4fec46bd85ee99fd
MarcinBalut <marcinmb13@tlen.pl>
2017-04-05 18:56:37 +0200

    Implement findMax function and test

--- app/app.js
+++ app/app.js
@@ -1,20 +1,22 @@
 function findMin(array)
-{
-    var min = array[0];
+    {
+        var min = array[0];
 
-    for(var i = 0; i < array.length; i++){
-        if(typeof array[i] != 'number'){
-            return false;
-        }
-        else{
-            if(array[i] < min){
-                min = array[i];
+        for(var i = 0; i < array.length; i++){
+            if (typeof array[i] !=  "number"){
+                return false;
+            }
+            else {
+                if(array[i] < min){
+                    min = array[i];}
             }
         }
+
+
+        return min;
+
     }
 
-    return min;
-}
 module.exports = {
     findMin: findMin
 };
--- tests/app.spec.js
+++ tests/app.spec.js
@@ -1,13 +1,13 @@
 var expect = require('chai').expect;
 var app = require('../app/app');
 
-
 it('When all the elements of the array are numbers, should return min element', function()
 {
     expect(app.findMin([2, 3, 0, 4])).to.eql(0);
 });
-
 it('When at least one of the array elements is not a number, should return false', function()
 {
     expect(app.findMin([2, 3, 'a', 4])).to.eql(false);
 });
+
+
