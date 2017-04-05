5774edb9ae45e7eaae94f7d01198d312b24307a0
root <m.podolanski@gmail.com>
2017-04-05 11:51:15 +0200

    Add findMax tests

---
+++ tests/app.spec.js
@@ -0,0 +1,27 @@
+var expect = require('chai').expect;
+var app = require('../app/app');
+
+describe('find maximum of array', function ()
+{
+    describe('when array is not empty and data are correct', function ()
+    {
+        it('should return correct value', function ()
+        {
+            expect(app.findMax([1, 2, 3, 4, -5 ])).to.eql(-5);
+        });
+    });
+    describe('when parameters are incorrect', function ()
+    {
+        it('should return false', function ()
+        {
+            expect(app.findMax(['ala', 'ma', 'kota'])).to.eql(false);
+        });
+    });
+    describe('when empty parameters', function ()
+    {
+        it('should return false', function ()
+        {
+            expect(app.findMax([])).to.eql(false);
+        });
+    });
+});
