16360e78c070f833064de7dafd953dd47116144f
icamer <maronicam@gmail.com>
2017-05-29 15:41:20 +0200

    findMin works

--- app/app.js
+++ app/app.js
@@ -1,6 +1,16 @@
 function findMin(array)
 {
-
+    if(!Array.isArray(array) || array.length==0)
+        return false;
+    var min=array[0], i=0;
+    while (array.length > i){
+        if (isNaN(array[i]))
+            return false;
+        i++;
+        if(array[i]<min)
+            min = array[i];
+    }
+    return min;
 }
 module.exports = {
     findMin: findMin
