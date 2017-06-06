c7687e03415a0a8ca9a6d0e710cbcb2ecd8642dd
Giena <giena@MacBook-Pro-Giena.local>
2017-06-06 17:40:42 +0200

    More tests

--- tests/app.spec.js
+++ tests/app.spec.js
@@ -4,12 +4,36 @@ var app = require('../app/app');
 describe('tests on develop branch', function () {
 
 
-    it('should check if it is a positve numbers', function () {
-        expect(app.areaOfTrapezoid(10, -5, 3)).to.eql(false);
-    });
+    it('should check if number is prime',function()
+    {
+        expect(app.isPrime(5)).to.eql(true);
+        expect(app.isPrime(6)).to.eql(false);
+    })
 
+    it('should return false when arg is not numeric or =< 0 ',function(){
+        expect(app.isPrime(-1)).to.eql(false);
+        expect(app.isPrime(0)).to.eql(false);
+        expect(app.isPrime('s')).to.eql(false);
 
-    it('should check if it is a prime number', function () {
-        expect(app.isPrime(13)).to.eql(true);
+    })
+
+    it('should calculate area of trapezoid',function () {
+        expect(app.areaOfTrapezoid(2,2,3)).to.eql(6);
+        expect(app.areaOfTrapezoid(3,5,5)).to.eql(20);
+    })
+    it('should return false when arg == 0',function(){
+        expect(app.areaOfTrapezoid(-1,2,3)).to.eql(false);
+        expect(app.areaOfTrapezoid(1,-2,3)).to.eql(false);
+        expect(app.areaOfTrapezoid(1,2,-3)).to.eql(false);
+        expect(app.areaOfTrapezoid(-1,-2,-3)).to.eql(false);
+        expect(app.areaOfTrapezoid(-1,-2,3)).to.eql(false);
+        expect(app.areaOfTrapezoid(-1,2,-3)).to.eql(false);
+        expect(app.areaOfTrapezoid(1,-2,-3)).to.eql(false);
+    })
+    it('should return fasle when arg is not numeric',function() {
+        expect(app.areaOfTrapezoid('a',2,3)).to.eql(false);
+        expect(app.areaOfTrapezoid(1,'a',3)).to.eql(false);
+        expect(app.areaOfTrapezoid(1,2,'a')).to.eql(false);
+        expect(app.areaOfTrapezoid('a','b','c')).to.eql(false);
     })
 });
