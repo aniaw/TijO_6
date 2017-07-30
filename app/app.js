b03fa73798c5f13cd080a18dbe66c4382bc7d798
Kamilszcz <dragon1393@wp.pl>
2017-07-30 13:54:15 +0200

    Implementacja funkcji findMax

--- app/app.js
+++ app/app.js
@@ -1,6 +1,16 @@
 function findMax(array)
 {
-
+    var max = array[0];
+    for(var i = 0; i < array.length; i++){
+        if (typeof array[i] != "number"){
+            return false;
+        }
+        else {
+            if(array[i] > max){
+                max = array[i];}
+        }
+    }
+    return max;
 }
 module.exports = {
     findMax: findMax
